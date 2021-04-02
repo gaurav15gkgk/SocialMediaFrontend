ba6f660590772a8ec9c53c289195c161bdc9633c
gaurav15gkgk <72983652+gaurav15gkgk@users.noreply.github.com>
2021-04-02 20:53:35 +0530

    navbar added and signout functionality added

---
+++ src/core/Menu.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+const isActive = (history, path) => {
+    if(history.location.pathname === path){
+        return {color: 'hsl(229, 53%,  53%)' ,
+                border:'1px solid hsl(0, 0%, 86%)',
+                
+            };
+    } 
+   
+    else return { color: "#363636"}
+   
+};
+
+const signout = next => {
+    if (typeof window !== "undefined") localStorage.removeItem("jwt");
+    next();
+    return fetch("http://localhost:8080/signout", {
+        method: "GET"
+    })
+        .then(response => {
+            
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+const Menu = ({ history }) =>(
+    <div className="tabs is-centered is-boxed mt-3">
+        <ul>
+            <li >
+                <Link  style ={isActive(history,'/')} to ="/">
+                   
+                    <span>Home</span>
+                </Link>
+            </li>
+            <li>
+                <Link style ={isActive(history,'/signup')} to  ='/signup'>
+                    
+                    <span>SignUp</span>
+                </Link>
+            </li>
+            <li>
+                <Link style ={isActive(history,'/signin')} to ='/signin'>
+                    
+                    <span>SignIn</span>
+                </Link>
+            </li>
+            <li>
+                <a href style ={isActive(history,'/signin'),
+                    {cursor: "pointer", color: "#363636"}
+                    
+                    }  onClick={() => signout(() => history.push("/"))}>
+                    
+                    <span>Signout</span>
+                </a>
+            </li>
+           
+        </ul>
+    </div>
+)
+
+export default withRouter(Menu)
