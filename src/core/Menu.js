711b372727e72954043ef066ff0abc3da6d5e82c
gaurav15gkgk <72983652+gaurav15gkgk@users.noreply.github.com>
2021-04-02 23:17:10 +0530

    conditional rendering and show of the user

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -26,6 +26,18 @@ const signout = next => {
         .catch(err => console.log(err));
 };
 
+const isAuthenticated = () => {
+    if (typeof window == "undefined") {
+        return false;
+    }
+
+    if (localStorage.getItem("jwt")) {
+        return JSON.parse(localStorage.getItem("jwt"));
+    } else {
+        return false;
+    }
+};
+
 const Menu = ({ history }) =>(
     <div className="tabs is-centered is-boxed mt-3">
         <ul>
@@ -35,30 +47,47 @@ const Menu = ({ history }) =>(
                     <span>Home</span>
                 </Link>
             </li>
-            <li>
-                <Link style ={isActive(history,'/signup')} to  ='/signup'>
-                    
-                    <span>SignUp</span>
-                </Link>
-            </li>
-            <li>
-                <Link style ={isActive(history,'/signin')} to ='/signin'>
-                    
-                    <span>SignIn</span>
-                </Link>
-            </li>
-            <li>
-                <a href style ={isActive(history,'/signin'),
-                    {cursor: "pointer", color: "#363636"}
-                    
-                    }  onClick={() => signout(() => history.push("/"))}>
-                    
-                    <span>Signout</span>
-                </a>
-            </li>
+            { !isAuthenticated() && 
+                <>
+                    <li>
+                        <Link style ={isActive(history,'/signup')} to  ='/signup'>
+                            
+                            <span>SignUp</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link style ={isActive(history,'/signin')} to ='/signin'>
+                            
+                            <span>SignIn</span>
+                        </Link>
+                    </li>
+                </>
+            }
+
+
+            {isAuthenticated() && 
+                <>
+                     <li>
+                        <a href style ={isActive(history,'/signin'),
+                            {cursor: "pointer", color: "#363636"}
+                            
+                            }  onClick={() => signout(() => history.push("/"))}>
+                            
+                            <span>Signout</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href >{isAuthenticated().user.name}</a>
+                    </li>
+           
+                </>
+            }
            
         </ul>
     </div>
 )
+
+
 
 export default withRouter(Menu)
